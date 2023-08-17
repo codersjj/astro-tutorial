2a89e4550770b773d50268e755ac414482e3c0c4
codersjj <shanewestlife@outlook.com>
2023-08-17 13:20:03 +0800

    update astro `base` config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codersjj.github.io',
-  base: '/astro-demo'
+  base: '/astro-tutorial'
 });
